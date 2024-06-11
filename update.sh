a1b51a4ab7fe28de4a05a6c9f087fabf0f639d45
DrSlime7 <49422667+Drslime7@users.noreply.github.com>
2024-06-11 13:29:03 +1000

    Update update.sh
    Added checks to make sure a package exist.

--- update.sh
+++ update.sh
@@ -1,5 +1,26 @@
 #! /bin/bash
 ## Basic update script that updates system with paru, syncs chezmoi and syncs pacdef
-paru
-chezmoi update
-pacded p sy
+
+command_exists() {
+    command -v $1 >/dev/null 2>&1
+}
+
+if ! command_exists paru; then
+        echo -e "To run me, you need: paru"
+        exit 1
+      else
+        paru
+    fi
+if ! command_exists chezmoi; then
+        echo -e "To run me, you need: chezmoi"
+        exit 1
+      else
+        chezmoi update
+    fi
+    
+if ! command_exists pacdef; then
+        echo -e "To run me, you need: pacdef"
+        exit 1
+      else
+        pacdef p sy
+    fi    
